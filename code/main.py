5122efe2f06e4bd86b5d9b6825c951bbc62e3a27
ozzyn13 <azin.zahrayi@gmail.com>
2020-10-25 14:55:44 +0100

    extra code removed

--- code/main.py
+++ code/main.py
@@ -1,54 +1,61 @@
 from data import Data
-from approaches.multithread import MultiThread
-from approaches.multiprocess import MultiProcess
 import time
-from utils.utils import timing_decorator
+from utils.utils import timing_decorator, email_is_valid
 import argparse
-import re
+import threading
+import queue
 
 from config import *
 
 
-# This is not a complete check for every possible valid email address
-# but catches most of the normal ones
-def email_is_valid(address):
-    return re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", address)
+class MultiThreadedRun:
+    def __init__(self, num_of_workers=10):
+        self.data = Data()
+        self.work = queue.Queue()
+        self.results = queue.Queue()
+        self.num_of_workers = num_of_workers
+        self.work_count = 0
 
+    def do_work(self):
+        while True:
+            item = self.work.get().decode('utf-8')
+            if not email_is_valid(item):
+                logging.info('invalid email address')
+                return
+            time.sleep(wait_time)  # instead of sending an email
+            self.results.put(item)
+            self.work.task_done()
 
-def send_email(address):
-    address = address.decode('utf-8')
-    # if not email_is_valid(address):
-    #     logging.info('invalid email address')
-    #     return
-    time.sleep(wait_time)
+    def start_workers(self):
+        for i in range(self.num_of_workers):
+            t = threading.Thread(target=self.do_work)
+            t.daemon = True
+            t.start()
+
+    def add_work(self, work):
+        self.work.put(work)
+        self.work_count += 1
+
+    def collect_results(self):
+        self.work.join()
+        for i in range(self.work_count):
+            logging.debug(self.results.get())
+
+    def run(self):
+        self.start_workers()
+        for item in self.data.iterate_items():
+            self.add_work(item)
+        self.collect_results()
 
 
 @timing_decorator
 def main(args):
-    data = Data()
-
-    if args.approach == 'multithread':
-        multithread = MultiThread(send_email, args.workers)
-        for item in data.iterate_items():
-            multithread.add_work(item)
-        multithread.collect_results()
-
-    if args.approach == 'multiprocess':
-        multiprocess = MultiProcess(send_email)
-        for item in data.iterate_with_sentinel(args.workers):
-            multiprocess.add_work(item)
-        multiprocess.collect_results()
-
-    if args.approach == 'not-parallel':
-        for item in data.iterate_items():
-            send_email(item)
-
+    multi_threaded_run = MultiThreadedRun(args.workers)
+    multi_threaded_run.run()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run program to send emails from a list')
-    parser.add_argument('--approach', '-a', type=str, required=False, default='multiprocess',
-                        help='Choose which approach to use to parallelize email sending')
-    parser.add_argument('--workers', '-w', type=int, required=False, default=10,
+    parser.add_argument('--workers', '-w', type=int, required=False, default=100,
                         help='Choose how many workers to run in parallel')
     main(parser.parse_args())
